c84678c9486388f717e29002260a8f2ff4cc8f83
Joey Troy <joey.troy@gmail.com>
2025-12-04 15:25:16 -0700

    Update license headers to match cursor rules specification
    Updated all license headers in source files to match the exact format
    specified in .cursor/rules/rules.mdc.
    
    Changes:
    - Changed 'version 1' to 'Version 1' (capitalized)
    - Updated license URL format to two-line format:
      - 'See the LICENSE file in this repository or the official license at:'
      - 'https://www.mongodb.com/legal/licensing/server-side-public-license'
    - Updated compliance text:
      - Changed 'If you use this library' to 'If you use this program'
      - Added 'appliance, or any commercial offering' to scope
      - Changed 'must comply with the SSPL v1 requirements' to
        'must comply with all SSPL v1 requirements'
    
    Updated 49 files:
    - 23 header files (.h)
    - 22 source files (.cpp)
    - 4 test files
    
    All license headers now conform to the project cursor rules specification.

--- src/drivers/switch_driver.cpp
+++ src/drivers/switch_driver.cpp
@@ -3,11 +3,13 @@
 //
 // This file is part of AlpacaCore.
 //
-// AlpacaCore is licensed under the Server Side Public License, version 1 (SSPL v1).
-// https://github.com/open-astro/AlpacaCore/blob/main/LICENSE
+// AlpacaCore is licensed under the Server Side Public License, Version 1 (SSPL v1).
+// See the LICENSE file in this repository or the official license at:
+// https://www.mongodb.com/legal/licensing/server-side-public-license
 //
-// If you use this library to provide a network-accessible service, you must comply
-// with the SSPL v1 requirements.
+// If you use this program to provide a network-accessible service, appliance,
+// or any commercial offering, you must comply
+// with all SSPL v1 requirements.
 
 #include <alpacacore/switch_driver.h>
 #include <alpacacore/alpaca_errors.h>
